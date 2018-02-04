0f9763e7562d8d47f9a934970dfd0d64f79e0ac2
Matthew Curry <matt@mattcurry.com>
2018-02-04 17:29:37 -0600

    Moving files/created Menu for calls

---
+++ jasper-rpi-tools.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+####
+#
+#   Jasper RPI Tools
+#   Author: Matthew Curry
+#   Description: Calls Various Jasper RPI Tools
+#
+####
+
+echo "####################"
+echo "# Jasper RPI Tools #"
+echo "####################"
+echo ""
+echo "    What would you like to do? "
+echo ""
+echo "1.  Install Jasper Repository. (Master OR Dev) "
+echo "2.  Download Modules Repository. (Master Only) "
+echo "3.  Test your audio.                (RPI Only) "
+echo "4.  Fix Audio (Bitrate)-BETA.                  "
+echo "5.  Install Jasper as Service.                 "
+echo "6.  Install Home Assistant/MQTT Server.        "
+echo "7.  Restart Jasper Service.                    "
+echo "8.  Stop Jasper Service.                       "
+echo "9.  Install Jasper Dependencies.   (Master/Dev)"
+echo "10. Convert Profile.yml to Dev Syntax.         "
+echo "11. Convert Profile.yml to Master Syntax.      "
+echo "12. Update RPI (OS and FW).                    "
+echo "13. Reboot RPI.                                "
+echo ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exit
